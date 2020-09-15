233a3daa4b850d9a2312c8e0116942319626486b
marcelomaciel <amominhache@yahoo.com.br>
2020-09-15 11:42:16 -0300

    commit new changes

--- example/bin/main.dart
+++ example/bin/main.dart
@@ -1,7 +1,6 @@
-import 'dart:convert';
-
-import 'package:example/config.dart';
+import 'package:example/config_exemple.dart';
 import 'package:orm_mysql/orm_mysql.dart';
+
 
 void main() async {
   MySQL db = MySQL(
@@ -12,11 +11,20 @@ void main() async {
   );
   await db.init();
   UserRepository repository = UserRepository();
+  LivroRepository livroRepository = LivroRepository();
+
   await repository.insert(User(
       name: 'Cristiano',
       age: 19,
       email: 'CpdnCristiano@gmail.com',
       isActive: null));
+
+  await livroRepository.insert(Livro(
+      name: 'Renascer da Esperança',
+      description: 'Livro de Romance',
+      userId: 1,
+      id: 1));
+
   List<User> allUser = await repository.findAll();
 
   print('Printando todos os usuários');
@@ -50,8 +58,10 @@ class User {
   int age;
   String email;
   bool isActive;
+  @foreignTable
+  Livro livro;
 
-  User({this.id, this.name, this.age, this.email, this.isActive});
+  User({this.id, this.name, this.age, this.email, this.isActive, this.livro});
 
   User.fromJson(Map<String, dynamic> json) {
     id = json['id'];
@@ -59,6 +69,7 @@ class User {
     age = json['age'];
     email = json['email'];
     isActive = json['isActive'];
+    livro = json['livro'] == null ? null : Livro.fromJson(json['livro']);
   }
 
   Map<String, dynamic> toJson() {
@@ -68,8 +79,40 @@ class User {
     data['age'] = age;
     data['email'] = email;
     data['isActive'] = isActive;
+    data['livro'] = livro.toJson();
     return data;
   }
 }
 
 class UserRepository extends Repository<User, int> {}
+
+@table
+class Livro {
+  @Id()
+  int id;
+  @foreignId
+  int userId;
+  String name;
+  String description;
+
+  Livro({this.id, this.userId, this.name, this.description});
+
+  Livro.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    userId = json['userId'];
+    name = json['name'];
+    description = json['description'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = Map<String, dynamic>();
+    data['id'] = id;
+    data['userId'] = userId;
+    data['name'] = name;
+    data['description'] = description;
+
+    return data;
+  }
+}
+
+class LivroRepository extends Repository<Livro, int> {}
--- lib/src/repository/interface/functions_repository.dart
+++ lib/src/repository/interface/functions_repository.dart
@@ -22,6 +22,8 @@ String _getColumnName(VariableMirror vm) {
     }
   });
  */
+
+  //mudando para contar _ para duas palvras no sql
   return ReCase(columnName).snakeCase;
 }
 
@@ -31,6 +33,7 @@ String _createTableFromObject(Type classe) {
   String tablename = _getTableName(cm);
   _getPrimaryKey(cm);
 
+  //TODO : livrar coluna com ForeignTable
   cm.metadata.forEach((meta) {
     if (meta.reflectee is Table) {
       query += 'CREATE TABLE IF NOT EXISTS $tablename (';
@@ -38,11 +41,15 @@ String _createTableFromObject(Type classe) {
         if (!value.isPrivate) {
           if (!(value is MethodMirror)) {
             VariableMirror vm = cm.declarations[key];
-            if (query.endsWith('(')) {
-              query += '\n ${_processColunm(vm)}';
-            } else {
-              query += ',\n ${_processColunm(vm)}';
-            }
+            vm.metadata.forEach((meta) {
+              if (!(meta.reflectee is ForeignTable)) {
+                if (query.endsWith('(')) {
+                  query += '\n ${_processColunm(vm)}';
+                } else {
+                  query += ',\n ${_processColunm(vm)}';
+                }
+              }
+            });
           }
         }
       });
@@ -53,6 +60,7 @@ String _createTableFromObject(Type classe) {
   return query;
 }
 
+//TODO : adicionado FOREIGN
 String _processColunm(VariableMirror vm) {
   String column = _getColumnName(vm);
   column += ' ';
@@ -60,6 +68,8 @@ String _processColunm(VariableMirror vm) {
   vm.metadata.forEach((meta) {
     if (meta.reflectee is Id) {
       column += ' PRIMARY KEY AUTO_INCREMENT NOT NULL';
+    } else if (meta.reflectee is ForeignId) {
+      column += ' FOREIGN KEY REFERENCES';
     } else if (meta.reflectee is NotNull) {
       column += ' NOT NULL';
     }
@@ -84,6 +94,7 @@ String getSqlType(String dartType) {
 String _getPrimaryKey(ClassMirror cm) {
   String primarykey;
   int primarykeys = 0;
+
   cm.metadata.forEach((meta) {
     if (meta.reflectee is Table) {
       cm.declarations.forEach((key, value) {
@@ -107,7 +118,52 @@ String _getPrimaryKey(ClassMirror cm) {
   if (primarykeys > 1) {
     throw Exception('More than one @id was entered in the ${cm.location} ');
   }
+
   return primarykey;
+}
+//TODO : pegando ForeignId das classes ForeignTable
+List<ForeignTableInner> _getForeignKeys(ClassMirror cm, Type T) {
+  var foreignkey = <ForeignTableInner>[];
+  cm.metadata.forEach((meta) {
+    if (meta.reflectee is Table) {
+      cm.declarations.forEach((key, value) {
+        if (!value.isPrivate) {
+          if (!(value is MethodMirror)) {
+            VariableMirror vm = cm.declarations[key];
+            vm.metadata.forEach((meta) {
+              if (meta.reflectee is ForeignTable) {
+                ClassMirror cmInner = reflectClass(vm.type.reflectedType);
+
+                cmInner.metadata.forEach((metaInner) {
+                  if (metaInner.reflectee is Table) {
+                    cmInner.declarations.forEach((key, value) {
+                      if (!value.isPrivate) {
+                        if (!(value is MethodMirror)) {
+                          if (metaInner.reflectee is ForeignId) {
+                            //foreignkey.add(_getColumnName(vm));
+                            VariableMirror vmInner = cmInner.declarations[key];
+                            foreignkey.add(ForeignTableInner(
+                                name: _getColumnName(vm),
+                                foreignId: _getColumnName(vmInner)));
+                          }
+                        }
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          }
+        }
+      });
+    }
+  });
+
+  if (foreignkey.isEmpty) {
+    throw Exception('Annotations @ForeignId not found in ${cm.location} ');
+  }
+
+  return foreignkey;
 }
 
 void _replaceBoolInt(ClassMirror cm, Map<String, dynamic> data) {
